70e08231a1c8fb52ac046fa430dad39702a90fbf
sohel5G <105277301+sohel5G@users.noreply.github.com>
2023-09-28 15:01:32 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { Link, Outlet, useLocation } from 'react-router-dom'
 import './App.css'
+import { useEffect } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const location = useLocation()
+
+  useEffect(() => {
+    location.pathname === '/' ? 
+    document.title = `Title - Home` : 
+    document.title = `Title ${location.pathname.replace('/', '- ')}`;
+
+    location.state ? document.title = `Title - ${location.state}` : ''
+  }, [location])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <nav>
+        <li> <Link to={'/'}>Home</Link> </li>
+        <li> <Link to={'/about'}>About</Link> </li>
+        <li> <Link to={'/contact'}>Contact</Link> </li>
+        <li> <Link to={'/single'} state={'Single Page products title'}> single page </Link> </li>
+      </nav>
+
+      <Outlet></Outlet>
+
     </>
   )
 }
